891ef2bd18f1adf39a0f8f5e6bff11ae84d8b258
DINKONDBD <70340226+DINKONDBD@users.noreply.github.com>
2022-02-23 19:02:38 +0200

    Added files

---
+++ Script.js
@@ -0,0 +1,28 @@
+let result = "";
+let isGood = null;
+function hasNumber(myString) {
+    return /\d/.test(myString);
+  }
+function isUpper(str) {
+    return !/[a-z]/.test(str) && /[A-Z]/.test(str);
+}
+function containsSpecialChars(str) {
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return specialChars.test(str);
+  }
+function Check(){
+    console.log("checking");
+    if(document.getElementById("UserPass").value.length > 16 || containsSpecialChars(document.getElementById("UserPass").value) && document.getElementById("UserPass").value.length > 12 || document.getElementById("UserPass").value.length > 10 && hasNumber(document.getElementById("UserPass").value)){
+        isGood = true;
+        result = "Recommended"
+        document.getElementById("result").innerHTML = result;
+        document.getElementById("result").style.color = "Blue";
+    }
+    else{
+        isGood = false;
+        result = "Not recommended"
+        document.getElementById("result").innerHTML = result;
+        document.getElementById("result").style.color = "red";
+    }
+    
+}
